b1d76632b989b991fc137b83abbc8480909bead7
nico <ncls.ayala@gmail.com>
2015-01-22 23:26:37 -0300

    Added sinatra-asset-pipeline gem. Added rerun gem in test and development environments (see README). Created vendor folder to put third party assets in there.

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # Tokyo
 
 - Ruby
+- Sinatra
 - Sass
 - Coffee
 - Haml
+
+## For development
+
+    $ git clone git@github.com:lucasdelrio/tokyo.git
+    $ bundle
+    $ rerun rackup
--- app.rb
+++ app.rb
@@ -1,9 +1,33 @@
+require 'bundler'
+Bundler.require(:default)
+
+require 'sinatra/base'
+require 'sinatra/asset_pipeline'
+
 class App < Sinatra::Base
+
   set :root, File.dirname(__FILE__)
   set :views, Proc.new { File.join(root, "app/views") }
   set :logging, true
 
+  # in development, serve individual files
+  configure :test, :development do
+    Sprockets::Helpers.configure do |config|
+      config.debug = true
+    end
+  end
+
+  # Include these files when precompiling assets
+  set :assets_precompile, %w(application.sass application.coffee)
+
+  # Logical paths to your assets
+  set :assets_prefix, %w(app/assets vendor/assets)
+
+  # integrate Sprockets
+  register Sinatra::AssetPipeline
+
   get '/' do
     haml :application
   end
+
 end
